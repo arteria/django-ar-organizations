a9e97121d28c17c4a807e9aa57f82a7d9874f887
Philippe O. Wagner <wagner@arteria.ch>
2012-08-19 22:03:17 +0200

    no message

--- organizations/utils.py
+++ organizations/utils.py
@@ -21,4 +21,7 @@ def model_field_attr(model, model_field, attr):
     Returns the specified attribute for the specified field on the model class.
     """
     fields = dict([(field.name, field) for field in model._meta.fields])
-    return getattr(fields[model_field], attr)
+    try:
+        return getattr(fields[model_field], attr)
+    except Exception, ex:
+        return "ERROR " + str(ex) 
