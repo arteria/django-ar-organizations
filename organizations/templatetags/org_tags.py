572a2b52c8b6e53ac5ba9a37f0594a8be328e6a2
Walter Renner <renner@arteria.ch>
2014-09-29 13:11:01 +0200

    new templatetag "users_organizations"

--- organizations/templatetags/org_tags.py
+++ organizations/templatetags/org_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from organizations.utils import get_users_organizations
 register = template.Library()
 
 
@@ -7,3 +8,17 @@ register = template.Library()
 def organization_users(context, org):
     context.update({'organization_users': org.organization_users.all()})
     return context
+
+
+@register.assignment_tag
+def users_organizations(user):
+    """
+    Returns all organizations, in wich the user is member.
+    Use in Template:
+    {% load org_tags %}
+    {% users_organizations request.user as my_orgs %}
+    """
+    if not user or not user.is_authenticated():
+        return None
+    else:
+        return get_users_organizations(user)
