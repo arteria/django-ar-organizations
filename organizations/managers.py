0376957858a9e3eef83cda1a93240232876c68d2
Walter Renner <renner@arteria.ch>
2014-11-17 16:55:49 +0100

    Renamed get_query_set to get_queryset
    get_query_set in deprecated in Django 1.8

--- organizations/managers.py
+++ organizations/managers.py
@@ -4,7 +4,7 @@ from django.db import models
 class OrgManager(models.Manager):
 
     def get_for_user(self, user):
-        return self.get_query_set().filter(users=user)
+        return self.get_queryset().filter(users=user)
 
 
 class ActiveOrgManager(OrgManager):
@@ -13,6 +13,6 @@ class ActiveOrgManager(OrgManager):
     including only active organizations
     """
 
-    def get_query_set(self):
+    def get_queryset(self):
         return super(ActiveOrgManager,
-                self).get_query_set().filter(is_active=True)
+                self).get_queryset().filter(is_active=True)
