f82d8801f58c37683ef27f1866dc52592c47b4a6
Walter Renner <renner@arteria.ch>
2015-01-28 14:24:46 +0100

    Provide a get_query_set fallback that works

--- organizations/managers.py
+++ organizations/managers.py
@@ -5,7 +5,10 @@ from django.db import models
 class OrgManager(models.Manager):
 
     def get_for_user(self, user):
-        return self.get_queryset().filter(users=user)
+        if django.VERSION < (1, 7):
+            return self.get_query_set().filter(users=user)
+        else:
+            return self.get_queryset().filter(users=user)
 
 
 class ActiveOrgManager(OrgManager):
@@ -14,9 +17,14 @@ class ActiveOrgManager(OrgManager):
     including only active organizations
     """
 
-    def get_queryset(self):
-        return super(ActiveOrgManager,
-                self).get_queryset().filter(is_active=True)
+    def get_queryset(self, request):
+        super_ = super(ActiveOrgManager, self)
+        if django.VERSION < (1, 7):
+            qs = super_.get_query_set(request)
+        else:
+            qs = super_.get_queryset(request)
 
-    if django.VERSION < (1, 6):
+        return qs.filter(is_active=True)
+
+    if django.VERSION < (1, 7):
         get_query_set = get_queryset
