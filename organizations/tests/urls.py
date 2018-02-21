1ac1d6e2a8170515a77fda91a4ed5671b4809429
Walter Renner <renner@arteria.ch>
2018-02-21 11:29:26 +0100

    fix url includes in tests

--- organizations/tests/urls.py
+++ organizations/tests/urls.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-from compat import include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from organizations.backends import invitation_backend, registration_backend
 
-admin.autodiscover()
-
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^organizations/', include('organizations.urls')),
     url(r'^invite/', include(invitation_backend().get_urls())),
     url(r'^register/', include(registration_backend().get_urls())),
